0c442f4bac0873cab0c7f3ca859d8bca341f2d11
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-21 01:36:56 -0700

    printf working for c and s and %

--- printf.c
+++ printf.c
@@ -7,14 +7,32 @@
 int _printf(const char *format, ...)
 {
 	unsigned int i, f;
+	va_list list;
 
+	va_start(list, format);
 	f = 0;
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 			_putchar(format[i]);
 		else
+		{
 			f++, i++;
+			switch (format [i])
+			{
+				case 'c':
+					_putchar(va_arg(list, int));
+					break;
+				case 's':
+					_putstring(va_arg(list, char *));
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					break;
+			}
+		}
 	}
+	va_end(list);
 	return (f);
 }
